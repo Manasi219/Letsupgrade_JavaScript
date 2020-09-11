9b92b9264c5213883dd8ff652dce6aec310c6be8
Manasi219 <64096110+Manasi219@users.noreply.github.com>
2020-09-11 12:30:01 +0530

    Add files via upload

---
+++ Day 4/Question 1/script2.js
@@ -0,0 +1,16 @@
+/*
+on clicking Button 2 Image would be change to light pink
+on clicking Button 3 Image would be change to dark pink
+*/
+function showImage()
+{
+	document.getElementById('image').src="download.jpg";
+}
+function showImg()
+{
+	document.getElementById('image').src="img.jpg";
+}
+function showImage1()
+{
+	document.getElementById('image').src="images.jpg"
+}
