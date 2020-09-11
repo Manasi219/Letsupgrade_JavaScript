7b7ca256c41bc31d1d872178f0070fd6e1b22878
Manasi219 <64096110+Manasi219@users.noreply.github.com>
2020-09-11 13:11:23 +0530

    Add files via upload

---
+++ Day 4/Question 3/script1.js
@@ -0,0 +1,30 @@
+
+let student =[
+	{
+		name:"ABC",
+		age:20,
+		country:"India",
+		hobbies:['reading','dancing'],
+	},
+	{
+		name:"Ajay",
+		age:50,
+		country:"America",
+		hobbies:['cooking','reading','dancing'],
+	},
+	{
+		name:"Krutika",
+		age:35,
+		country:"India",
+		hobbies:['singing','listening to music','sports'],
+	}
+
+
+];
+
+
+student.forEach(function (stud) {
+
+	// body...
+	console.log(stud);
+});
